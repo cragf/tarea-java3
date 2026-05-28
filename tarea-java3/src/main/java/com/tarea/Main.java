1e9e6ff47c6fe24180b1c29e23066bca884c42f0
cragf <carlosrobertoag0416@gmail.com>
2026-05-28 19:37:39 -0400

    Make it friendly

--- tarea-java3/src/main/java/com/tarea/Main.java
+++ tarea-java3/src/main/java/com/tarea/Main.java
@@ -5,13 +5,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sca = new Scanner(System.in); // un solo scanner se consume antes de acabar
+        Scanner sca = new Scanner(System.in);
         Scanner sca2 = new Scanner(System.in);
 
-        System.out.println("Ingrese los datos del perro: nombre, raza, fecha de nacimiento y peso");
+        System.out.println("Ingrese los datos del perro:");
+        System.out.print("Nombre: ");
         String nombrep = sca.nextLine();
+        System.out.print("Raza: ");
         String razap = sca.nextLine();
+        System.out.print("Fecha de nacimiento: ");
         String fechaNacimientop = sca.nextLine();
+        System.out.print("Peso: ");
         double pesop = sca.nextDouble();
 
         Perro perro1 = new Perro(nombrep, razap, fechaNacimientop, pesop);
@@ -19,11 +23,15 @@ public class Main {
         perro1.comunicarse();
 
 
-        System.out.println("Ingrese los datos del gato: nombre, raza, fecha de nacimiento y peso");
+        System.out.println("Ingrese los datos del gato:");
+        System.out.print("Nombre: ");
         String nombreg = sca2.nextLine();
+        System.out.print("Raza: ");
         String razag = sca2.nextLine();
+        System.out.print("Fecha de nacimiento: ");
         String fechaNacimientog = sca2.nextLine();
-        double pesog = sca.nextDouble();
+        System.out.print("Peso: ");
+        double pesog = sca2.nextDouble();
 
         Gato gato1 = new Gato(nombreg, razag, fechaNacimientog, pesog);
 
